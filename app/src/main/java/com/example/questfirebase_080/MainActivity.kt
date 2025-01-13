fe9cb698cb5621ed7b6d5715a69f315593cd105f
Egin Pardewan <eginpardewan@gmail.com>
2025-01-13 22:44:39 +0700

    update MainActivity.kt

--- app/src/main/java/com/example/questfirebase_080/MainActivity.kt
+++ app/src/main/java/com/example/questfirebase_080/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.questfirebase_080
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresExtension
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,17 +13,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.questfirebase_080.ui.navigation.PengelolaHalaman
 import com.example.questfirebase_080.ui.theme.QuestFirebase_080Theme
 
 class MainActivity : ComponentActivity() {
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             QuestFirebase_080Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    PengelolaHalaman(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
