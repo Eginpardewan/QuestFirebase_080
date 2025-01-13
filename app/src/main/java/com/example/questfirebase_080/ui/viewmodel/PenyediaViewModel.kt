0d61709b74cb442bf1a99200418ae224e0269b58
Egin Pardewan <eginpardewan@gmail.com>
2025-01-13 22:43:19 +0700

    membuat PenyediaViewModel.kt

---
+++ app/src/main/java/com/example/questfirebase_080/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.questfirebase_080.ui.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.questfirebase_080.MahasiswaApplication
+
+object PenyediaViewModel{
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
+        initializer { InsertViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
+    }
+}
+
+fun CreationExtras.aplikasiMahasiswa(): MahasiswaApplication =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplication)
